cb03be5d2fdb04fd99950bdd29ee39a24fa9a039
LBadalo <aluno19991@ipt.pt>
2020-10-21 10:58:07 +0100

    Criação dos controllers

---
+++ Controllers/Categoria_NoticiasController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ONoticiarioCore.Data;
+using ONoticiarioCore.Models;
+
+namespace ONoticiarioCore.Controllers
+{
+    public class Categoria_NoticiasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Categoria_NoticiasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categoria_Noticias
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Categoria_Noticias.Include(c => c.Categorias).Include(c => c.Noticias);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Categoria_Noticias/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoria_Noticias = await _context.Categoria_Noticias
+                .Include(c => c.Categorias)
+                .Include(c => c.Noticias)
+                .FirstOrDefaultAsync(m => m.CategoriaIdFK == id);
+            if (categoria_Noticias == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoria_Noticias);
+        }
+
+        // GET: Categoria_Noticias/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoriaIdFK"] = new SelectList(_context.Categorias, "ID", "ID");
+            ViewData["NoticiaIdFK"] = new SelectList(_context.Noticias, "ID", "Conteudo");
+            return View();
+        }
+
+        // POST: Categoria_Noticias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CategoriaIdFK,NoticiaIdFK")] Categoria_Noticias categoria_Noticias)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categoria_Noticias);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaIdFK"] = new SelectList(_context.Categorias, "ID", "ID", categoria_Noticias.CategoriaIdFK);
+            ViewData["NoticiaIdFK"] = new SelectList(_context.Noticias, "ID", "Conteudo", categoria_Noticias.NoticiaIdFK);
+            return View(categoria_Noticias);
+        }
+
+        // GET: Categoria_Noticias/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoria_Noticias = await _context.Categoria_Noticias.FindAsync(id);
+            if (categoria_Noticias == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoriaIdFK"] = new SelectList(_context.Categorias, "ID", "ID", categoria_Noticias.CategoriaIdFK);
+            ViewData["NoticiaIdFK"] = new SelectList(_context.Noticias, "ID", "Conteudo", categoria_Noticias.NoticiaIdFK);
+            return View(categoria_Noticias);
+        }
+
+        // POST: Categoria_Noticias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CategoriaIdFK,NoticiaIdFK")] Categoria_Noticias categoria_Noticias)
+        {
+            if (id != categoria_Noticias.CategoriaIdFK)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categoria_Noticias);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!Categoria_NoticiasExists(categoria_Noticias.CategoriaIdFK))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaIdFK"] = new SelectList(_context.Categorias, "ID", "ID", categoria_Noticias.CategoriaIdFK);
+            ViewData["NoticiaIdFK"] = new SelectList(_context.Noticias, "ID", "Conteudo", categoria_Noticias.NoticiaIdFK);
+            return View(categoria_Noticias);
+        }
+
+        // GET: Categoria_Noticias/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoria_Noticias = await _context.Categoria_Noticias
+                .Include(c => c.Categorias)
+                .Include(c => c.Noticias)
+                .FirstOrDefaultAsync(m => m.CategoriaIdFK == id);
+            if (categoria_Noticias == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoria_Noticias);
+        }
+
+        // POST: Categoria_Noticias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var categoria_Noticias = await _context.Categoria_Noticias.FindAsync(id);
+            _context.Categoria_Noticias.Remove(categoria_Noticias);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool Categoria_NoticiasExists(int id)
+        {
+            return _context.Categoria_Noticias.Any(e => e.CategoriaIdFK == id);
+        }
+    }
+}
---
+++ Controllers/ComentariosController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ONoticiarioCore.Data;
+using ONoticiarioCore.Models;
+
+namespace ONoticiarioCore.Controllers
+{
+    public class ComentariosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ComentariosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Comentarios
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Comentarios.Include(c => c.Noticia).Include(c => c.Utilizador);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Comentarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentarios = await _context.Comentarios
+                .Include(c => c.Noticia)
+                .Include(c => c.Utilizador)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (comentarios == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Create
+        public IActionResult Create()
+        {
+            ViewData["NoticiasFK"] = new SelectList(_context.Noticias, "ID", "Conteudo");
+            ViewData["UtilizadorFK"] = new SelectList(_context.Utilizadores, "ID", "ID");
+            return View();
+        }
+
+        // POST: Comentarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Descricao,UtilizadorFK,Data,NoticiasFK")] Comentarios comentarios)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(comentarios);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NoticiasFK"] = new SelectList(_context.Noticias, "ID", "Conteudo", comentarios.NoticiasFK);
+            ViewData["UtilizadorFK"] = new SelectList(_context.Utilizadores, "ID", "ID", comentarios.UtilizadorFK);
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentarios = await _context.Comentarios.FindAsync(id);
+            if (comentarios == null)
+            {
+                return NotFound();
+            }
+            ViewData["NoticiasFK"] = new SelectList(_context.Noticias, "ID", "Conteudo", comentarios.NoticiasFK);
+            ViewData["UtilizadorFK"] = new SelectList(_context.Utilizadores, "ID", "ID", comentarios.UtilizadorFK);
+            return View(comentarios);
+        }
+
+        // POST: Comentarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Descricao,UtilizadorFK,Data,NoticiasFK")] Comentarios comentarios)
+        {
+            if (id != comentarios.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comentarios);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ComentariosExists(comentarios.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NoticiasFK"] = new SelectList(_context.Noticias, "ID", "Conteudo", comentarios.NoticiasFK);
+            ViewData["UtilizadorFK"] = new SelectList(_context.Utilizadores, "ID", "ID", comentarios.UtilizadorFK);
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentarios = await _context.Comentarios
+                .Include(c => c.Noticia)
+                .Include(c => c.Utilizador)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (comentarios == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentarios);
+        }
+
+        // POST: Comentarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var comentarios = await _context.Comentarios.FindAsync(id);
+            _context.Comentarios.Remove(comentarios);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ComentariosExists(int id)
+        {
+            return _context.Comentarios.Any(e => e.ID == id);
+        }
+    }
+}
